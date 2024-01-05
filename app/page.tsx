685e34822f245feedeb2862a69225c293c61321b
Okellothomas <thomasolal33@gmail.com>
2024-01-05 13:22:19 +0300

    add more content

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,21 @@
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-3xl font-bold underline">
+      <h1 className="text-3xl font-bold underline bg-green-500 text-red-950">
       Hello world!
-     </h1>
+      </h1>
+      <h2>
+        This is the second title, and we just like it much much
+      </h2>
+      <hr />
+      <h2 className="border bg-green-600">
+        <ol className="text-veujs-100 sm:text-sm lg:text-lg md:text-xl xl:text-base 2xl: text-3xl">
+          <li className="md-3">Okello Thomas is the man here ok!</li>
+          <li>Okello Thomas is the man here ok!</li>
+          <li>Okello Thomas is the man here ok!</li>
+          <li>Okello Thomas is the man here ok!</li>
+        </ol>
+      </h2>
     </main>
   )
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,11 +7,41 @@ const config: Config = {
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    spacing: {
+      '1': '8px',
+      '2': '12px',
+      '3': '16px',
+      '4': '24px',
+      '5': '32px',
+      '6': '48px',
+    },
+    screens: {
+      'sm': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
+    },
     extend: {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+      colors: {
+        veujs: {
+          100: "#333",
+          900: "#020"
+        }
       },
     },
   },
